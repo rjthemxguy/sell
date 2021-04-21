7e62d5dbb7e94e3772241bdc5054b1d6c5ae23b7
RJ <rj@localhost.localdomain>
2021-04-20 18:45:47 -0700

    layout start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
+import HomePage from "./homepage.component";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <HomePage/>
       
-      </header>
+      
     </div>
   );
 }
---
+++ src/homepage.component.jsx
@@ -0,0 +1,53 @@
+
+
+
+const HomePage = () => (
+
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <h1>Hats</h1>
+                        <span>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <h1>Jackets</h1>
+                        <span>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <h1>Sneakers</h1>
+                        <span>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <h1>Womens</h1>
+                        <span>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <h1>Mens</h1>
+                        <span>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+)
+
+export default HomePage;
