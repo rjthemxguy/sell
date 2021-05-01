8712037b94d679d1159fa1250d0e71d0ddfe3335
RJ <rj@localhost.localdomain>
2021-05-01 01:09:56 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 
+
+const HatsPage = () => (
+<div>
+
+  <h1>Hats are here</h1>
+</div>
+
+);
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
-      
+      <Switch>
+        <Route exact path = "/" component = {HomePage}/> 
+        <Route exact path = "/hats" component = {HatsPage}/> 
+      </Switch>
       
     </div>
   );
