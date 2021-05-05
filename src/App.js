14c49d5439859a956b1835881aa3ee63cddabe90
RJ <rj@localhost.localdomain>
2021-05-05 01:44:13 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
@@ -10,6 +11,7 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Switch>
         <Route exact path = "/" component = {HomePage}/> 
         <Route exact path = "/shop" component = {ShopPage}/> 
