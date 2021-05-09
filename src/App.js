75d6239bd94711a3c6fb790024542221428206e6
RJ Robinson <rj@localhost.localdomain>
2021-05-08 17:08:33 -0700

    Google Auth User

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-
+import React from "react";
 import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import Header from './components/header/header.component';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import {auth} from "./firebase/firebase.utils";
 
 
 
 
-function App() {
+class App extends React.Component {
+
+constructor() {
+    super();
+
+    this.state = {
+      currentUser : null
+    }
+  }
+
+unsubscribeFromAuth = null;
+
+componentDidMount() {
+this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+  this.setState({currentUser: user})
+})
+}
+
+componentWillUnmount() {
+  this.unsubscribeFromAuth();
+}
+
+render() {
+
   return (
     <div className="App">
       <Header></Header>
@@ -21,6 +45,9 @@ function App() {
       
     </div>
   );
+
+}
+
 }
 
 export default App;
