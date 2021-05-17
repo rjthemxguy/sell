1db1c2a39b5582883dbb74c8a7635c89094d63cb
RJ Robinson <rj@localhost.localdomain>
2021-05-17 14:22:06 -0700

    redux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import  {provider} from "react-redux";
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-   
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+   </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
