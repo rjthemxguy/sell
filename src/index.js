6cd74c6d6f3c94c7e39885e7f88fd9c172321715
RJ Robinson <rj@localhost.localdomain>
2021-05-17 14:42:09 -0700

    redux fix

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import  {provider} from "react-redux";
 import { Provider } from 'react-redux';
-
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store = {store} >
       <BrowserRouter>
         <App />
       </BrowserRouter>
